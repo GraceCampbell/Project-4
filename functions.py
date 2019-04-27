9a359a21109fb5379ac9f807e51bb2b21d50fa65
Kevin Kim <kevinkim0817@gmail.com>
2019-04-27 17:06:14 -0400

    added library imports

--- functions.py
+++ functions.py
@@ -3,6 +3,11 @@
 
 # In[ ]:
 
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Functions
 def make_datetime(df):
